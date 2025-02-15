c17c4cf0ddf354328f194697f5c621ca550a6bfe
romanornr <romanornr@gmail.com>
2025-02-15 22:10:12 +0100

    Add methods for location ID retrieval and payment intent creation
    Introduce `getLocationId` to fetch the location ID and `createPaymentIntent` to handle payment intents with amount and currency details. Default configuration is now used in the absence of a provided TerminalConfig.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,15 @@
 // src/services/api-client.ts
 
 import axios, { AxiosInstance } from 'axios';
-import type { TerminalConfig } from '@/config/config';
+import { DEFAULT_CONFIG, TerminalConfig } from '@/config/config';
 import type { Logger } from '@/logger/Logger';
+import type { PaymentIntent, PaymentIntentResponse } from '@/types';
 
 export class ApiClient {
   private readonly client: AxiosInstance;
 
   constructor(
-    private readonly config: TerminalConfig,
+    private readonly config: TerminalConfig = DEFAULT_CONFIG,
     private readonly logger: Logger
   ) {
     this.client = axios.create({
@@ -39,4 +40,33 @@ export class ApiClient {
     }
   }
 
+  async getLocationId(): Promise<string> {
+    try {
+      this.logger.debug('Fetching location ID from', this.config.baseUrl);
+      const { data } = await this.client.get<{ data?: { location_id?: string } }>(
+        this.config.endpoints.locationId
+      )
+
+      this.logger.debug('Location ID:', data);
+      if (!data?.data?.location_id) throw new Error('Invalid response structure');
+      return data.data.location_id;
+    } catch (error) {
+      this.logger.error('Error fetching location ID:', error);
+      throw error;
+    }
+  }
+
+  async createPaymentIntent(amount: number, currency: string = this.config.currency): Promise<PaymentIntentResponse> {
+    try {
+      const amountInCents = Math.round(amount * 100);
+      this.logger.debug('Creating payment intent...', { amountInCents, currency});
+      const { data } = await this.client.post<{data?: PaymentIntent }> (
+        this.config.endpoints.paymentIntent, { amount: amountInCents, currency }
+      );
+      if (!data?.data?.client_secret) throw new Error('Invalid payment intent response');
+      return { success: true, data: data.data }
+    } catch (error) {
+      return { success: false, error: error as string || 'An unknown error occurred' };
+    }
+  }
 }
