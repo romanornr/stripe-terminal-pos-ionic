d1a107bf464306fbb8c8836cea47a3c4bd6d05c6
romanornr <romanornr@gmail.com>
2025-04-02 23:52:38 +0200

    update name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'stripe-terminal-pos-ionic',
+  appName: 'stripe-terminal',
   webDir: 'dist',
   plugins: {
     SplashScreen: {
